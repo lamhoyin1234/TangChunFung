e9b6114283ae74af3de9ae01c63edc70633e4d6e
Lam Ho Yin <lamlamhohoyinyin@hotmail.com>
2025-02-14 01:00:00 +0000

    Update Firebase database import and enhance firebaseGetJSON function for data retrieval

--- js/firebase.js
+++ js/firebase.js
@@ -1,27 +1,39 @@
-function firebaseGetJSON(path, objectname, callback, fallback){
-    var parsedPath=UTF8ToString(path);
-    var parsedObjectname=UTF8ToString(objectname);
-    var parsedCallback=UTF8ToString(callback);
-    var parsedFallback=UTF8ToString(fallback);
-    try{
+function firebaseGetJSON(path, objectname, callback, fallback) {
+    var parsedPath = UTF8ToString(path);
+    var parsedObjectname = UTF8ToString(objectname);
+    var parsedCallback = UTF8ToString(callback);
+    var parsedFallback = UTF8ToString(fallback);
+    try {
+        const dbRef = ref(database);
+        get(child(dbRef, `users/${userId}`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                alert(snapshot.val());
+            } else {
+                alert("No data available");
+            }
+        }).catch((error) => {
+            alert(error);
+        });
+        /*
         var starCountRef = window.firebase.database().ref(parsedPath);
         starCountRef.on('value', (snapshot) => {
             const data = snapshot.val();
             alert(JSON.stringify(data));
             //unityInstance.SendMessage(parsedObjectname, parsedCallback, JSON.stringify(data));
         });
+        */
         /*
         window.database.ref(parsedPath).once('value').then(function(snapshot) {
             window.unityInstance.SendMessage(parsedObjectname, parsedCallback, JSON.stringify(snapshot.val()));
         });
         */
     }
-    catch(error){
-    
-        //unityInstance.SendMessage(parsedObjectname,parsedFallback, JSON.stringify("FirebaseJSlib error: "+error, Object.getOwnPropertyNames(error)));
-        alert(JSON.stringify("Unity FirebaseJSlib error: "+error, Object.getOwnPropertyNames(error)));
+    catch (error) {
 
-    
+        //unityInstance.SendMessage(parsedObjectname,parsedFallback, JSON.stringify("FirebaseJSlib error: "+error, Object.getOwnPropertyNames(error)));
+        alert(JSON.stringify("Unity FirebaseJSlib error: " + error, Object.getOwnPropertyNames(error)));
+
+
     }
 
 }
