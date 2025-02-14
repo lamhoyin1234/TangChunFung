be1d52b3b05c3fe3945576d98c030d25679773ec
Lam Ho Yin <lamlamhohoyinyin@hotmail.com>
2025-02-14 00:45:23 +0000

    Add Firebase integration and alert handling in index.html and firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,27 @@
+function firebaseGetJSON(path, objectname, callback, fallback){
+    var parsedPath=UTF8ToString(path);
+    var parsedObjectname=UTF8ToString(objectname);
+    var parsedCallback=UTF8ToString(callback);
+    var parsedFallback=UTF8ToString(fallback);
+    try{
+        var starCountRef = window.firebase.database().ref(parsedPath);
+        starCountRef.on('value', (snapshot) => {
+            const data = snapshot.val();
+            alert(JSON.stringify(data));
+            //unityInstance.SendMessage(parsedObjectname, parsedCallback, JSON.stringify(data));
+        });
+        /*
+        window.database.ref(parsedPath).once('value').then(function(snapshot) {
+            window.unityInstance.SendMessage(parsedObjectname, parsedCallback, JSON.stringify(snapshot.val()));
+        });
+        */
+    }
+    catch(error){
+    
+        //unityInstance.SendMessage(parsedObjectname,parsedFallback, JSON.stringify("FirebaseJSlib error: "+error, Object.getOwnPropertyNames(error)));
+        alert(JSON.stringify("Unity FirebaseJSlib error: "+error, Object.getOwnPropertyNames(error)));
+
+    
+    }
+
+}
