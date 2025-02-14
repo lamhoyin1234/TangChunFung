69929d1b36797988f46970c15fd0d9a1648fd82e
Lam Ho Yin <lamlamhohoyinyin@hotmail.com>
2025-02-14 02:40:42 +0000

    Refactor Firebase initialization to module format and move configuration to firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, child, get } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-database.js"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA_2yd3_KIu23n2qDnL4McPL51qBqhTD-w",
+    authDomain: "sample-8e49f.firebaseapp.com",
+    databaseURL: "https://sample-8e49f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "sample-8e49f",
+    storageBucket: "sample-8e49f.firebasestorage.app",
+    messagingSenderId: "57010539033",
+    appId: "1:57010539033:web:eca0a64a97e954c0f7f888"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+alert("Firebase: " + firebase.name + " and db: " + database.name);
+database = getDatabase();
+alert("Firebase: " + firebase.name + " and db: " + database.name);
+
 function firebaseGetJSON(path, objectname, callback, fallback) {
     var parsedPath = UTF8ToString(path);
     var parsedObjectname = UTF8ToString(objectname);
